0c0d1ed1c571cd6594183f0ac21aa54c63991e13
Endre Karlson <endre.karlson@gmail.com>
2012-06-06 14:38:13 +0200

    Initial layout

---
+++ bookie/scripts/initializedb.py
@@ -0,0 +1,126 @@
+import os
+import sys
+import transaction
+from pprint import pformat
+
+from random import choice
+
+from sqlalchemy import engine_from_config
+
+from pyramid.paster import get_appsettings, setup_logging
+
+from ..models import (
+    configure_db, User, UserPermission, Group, GroupPermission, Resource,
+    Customer, DrivableEntity, Category, Property
+)
+
+
+def usage(argv):
+    cmd = os.path.basename(argv[0])
+    print('usage: %s <config_uri>\n'
+          '(example: "%s development.ini")' % (cmd, cmd))
+    sys.exit(1)
+
+
+def main(argv=sys.argv):
+    if len(argv) != 2:
+        usage(argv)
+    config_uri = argv[1]
+    setup_logging(config_uri)
+    settings = get_appsettings(config_uri)
+
+    configure_db(settings)
+
+    load_samples()
+
+
+def category_dict(names):
+    #print names
+    return dict([(n, Category(name=n)) for n in names])
+
+
+def load_samples():
+    with transaction.manager:
+        g_admin = GroupPermission(perm_name="admin")
+        g_edit = GroupPermission(perm_name="edit")
+        g_view = GroupPermission(perm_name="view")
+        g_order = GroupPermission(perm_name="order")
+
+        # NOTE: First Retailer rents out cars and transport class cars
+        customer = Customer(name="WreckRenters Inc", organisation_id=2343,
+                    contact="Steffen Soma", email="stefsoma@gmail.com",
+                    phone="+47 xxxxxxxx")
+
+        user = User(user_name="booker1", status=1,
+                    email="endre.karlson@gmail.com")
+        user.set_password("booker1")
+        user.save()
+
+        group = Group(
+            group_name="RentOurWrecks Inc",
+            users=[user],
+            permissions=[g_admin],
+            organisation_id=3232,
+            customers=[customer]).save()
+
+        sub_cars = category_dict(["Sedan", "Stationwagon", "SUV", "Coupe",
+                            "Pickup", "7 seats", "9 seats"])
+        top_cars = Category(name="Cars", categories=sub_cars.values()).save()
+        sub_transport = category_dict(["Caddy", "6m 3", "11 m3", "18 m3"])
+        top_transport = Category(name="Transport",
+                                categories=sub_transport.values()).save()
+        resources = Resource(
+            resource_name="Cars", categories=[top_cars, top_transport],
+            resource_type="categories").save()
+
+        entity = DrivableEntity(
+            brand="Mitsubishi",
+            model="Lancer",
+            identifier="DL87162",
+            produced=2008,
+            categories=[sub_cars["SUV"]]).save()
+        entity = DrivableEntity(
+            brand="Hyundai",
+            model="Elantra",
+            identifier="RH73369",
+            produced=2000,
+            categories=[sub_cars["Stationwagon"]]).save()
+
+        # NOTE: Rents out Trucks and Bus
+        customer = Customer(name="WreckLovers Inc", organisation_id=2343,
+                contact="Endre Karlson", email="endre.karlson@gmail.com",
+                phone="+47 xxxxxxxx")
+
+        user = User(user_name="booker2", status=1,
+                    email="endre.karlson@bouvet.no").save()
+        user.set_password("admin")
+        user.save()
+
+        group = Group(
+            group_name="TransportVehicles Inc",
+            users=[user],
+            organisation_id=3232,
+            permissions=[g_admin],
+            customers=[customer]).save()
+
+        sub_truck = category_dict(["18 m3", "22 m3", "26 m3", "35 m3"])
+        top_truck = Category(name="Truck", categories=sub_truck.values()).save()
+        sub_bus = category_dict(["9 seats", "17 seats"])
+        top_bus = Category(name="Mini bus", categories=sub_bus.values()).save()
+        resources = Resource(
+            resource_name="Transport", categories=[top_truck, top_bus],
+            resource_type="categories").save()
+
+        entity = DrivableEntity(
+            brand="Brand-A",
+            model="Model-A",
+            identifier="ID-A",
+            produced=2004,
+            categories=[sub_truck["18 m3"]]).save()
+        entity = DrivableEntity(
+            brand="Brand-B",
+            model="Model-B",
+            identifier="ID-B",
+            produced=2011,
+            categories=[sub_bus["9 seats"]]).save()
+
---
+++ bookie/security.py
@@ -0,0 +1,50 @@
+import logging
+from pyramid import httpexceptions as exception
+from pyramid.security import unauthenticated_userid, Authenticated, Allow, \
+    Everyone
+
+from . import models
+
+LOG = logging.getLogger(__name__)
+
+
+def get_group(user_id, request):
+    if user_id and hasattr(request, 'user'):
+        db_groups = ['group:%s' % g.group_name for g in request.user.groups]
+    groups = db_groups if len(db_groups) > 0 else []
+    LOG.info("Groups for %s set to %s" % (user_id, ", ".join(groups)))
+    return groups
+
+
+def add_user_to_request(request):
+    user = unauthenticated_userid(request)
+    if user:
+        return models.User.by_user_name(user)
+
+
+class ResourceFactory(object):
+    def __init__(self, request):
+        self.__acl__ = []
+        application_id = request.matchdict.get("resource_id") \
+                         or request.params.get("resource_id")
+
+        #if not application_id:
+        #    raise exception.HTTPNotFound
+        self.resource = models.Resource.by_resource_id(application_id)
+        if not self.resource:
+            raise exception.HTTPNotFound
+        if self.resource and request.user:
+            self.__acl__ = self.resource.__acl__
+            for perm_user, perm_name in self.resource.perms_for_user(request.user):
+                self.__acl__.append((Allow, perm_user, perm_name,))
+
+
+class RootFactory(object):
+    def __init__(self, request):
+        self.__acl__ = [(Allow, Authenticated, u'view'), ]
+        #general page factory - append custom non resource permissions
+        if request.user:
+            for perm_user, perm_name in request.user.permissions:
+                self.__acl__.append((Allow, perm_user, perm_name,))
+        pass
+
