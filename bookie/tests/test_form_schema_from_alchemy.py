1041e9f176470d2679cacf23bfcb42a71ca5bbea
Endre Karlson <endre.karlson@gmail.com>
2012-06-18 12:03:36 +0200

    Ok test. Should be fixed up with some checks

---
+++ bookie/tests/test_form_schema_from_alchemy.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from bookie.testing import Dummy, DummyRequest, UnitTestBase
+
+from bookie.db import models
+from bookie.views.helpers import AddFormView
+from bookie.views.bookings import BookingSchema
+
+
+class TestForm(AddFormView):
+    item_type = "Test"
+
+
+class TestAddForm(UnitTestBase):
+    def test_something(self):
+        f = TestForm(Dummy(), DummyRequest(),
+                    schema=models.Booking.get_schema())()
