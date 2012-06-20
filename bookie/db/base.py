d57913dfe8320e785edc953f379b167f675863f9
Endre Karlson <endre.karlson@gmail.com>
2012-06-20 10:07:56 +0200

    Add from_dict a better way to update a record from a dict then .update (It takes relations as well)

--- bookie/db/base.py
+++ bookie/db/base.py
@@ -5,7 +5,7 @@ import ipdb
 from colanderalchemy import SQLAlchemyMapping
 from sqlalchemy import Column, Boolean, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import object_mapper, scoped_session, sessionmaker
+from sqlalchemy.orm import class_mapper, object_mapper, scoped_session, sessionmaker
 from sqlalchemy.orm.properties import ColumnProperty, RelationshipProperty
 from zope.sqlalchemy import ZopeTransactionExtension
 
@@ -109,7 +109,7 @@ class BaseModel(object):
         data = dict([(k, getattr(self, k)) \
             for k in get_prop_names(self, exclude=exclude)[0]])
 
-        for rname, rdeep in deep.iteritems():
+        for rname, rdeep in deep.items():
             data.update(self.remote_to_dict(rname, rdeep))
         return data
 
@@ -190,5 +190,66 @@ class BaseModel(object):
     def get_schema(cls):
         return SQLAlchemyMapping(cls)
 
+    def from_dict(self, data):
+        """
+        Update a mapped class with data from a JSON-style nested
+        dict/list
+        """
+        mapper = object_mapper(self)
+        for key, value in data.items():
+            if isinstance(value, dict):
+                dbvalue = getattr(self, key)
+                rel_class = mapper.get_property(key).mapper.class_
+                pk_props = rel_class.__mapper__.primary_key
+
+                # If the data doesn't contain any pk, and the relationship
+                # already has a value, update that record.
+                if not [1 for p in pk_props if p.key in data] and \
+                        dbvalue is not None:
+                    dbvalue.from_dict(value)
+                else:
+                    record = rel_class.update_or_create(value)
+                    setattr(self, key, record)
+            elif isinstance(value, list) and \
+                    value and isinstance(value[0], dict):
+                rel_class = mapper.get_property(key).mapper.class_
+                new_attr_value = []
+                for row in value:
+                    if not isinstance(row, dict):
+                        raise Exception(
+                            'Cannot send mixed (dict/non dict) data '
+                            'to list relationships in from_dict data.')
+                    record = rel_class.update_or_create(row)
+                    print record
+                    new_attr_value.append(record)
+                setattr(self, key, new_attr_value)
+            else:
+                setattr(self, key, value)
+        return self
+
+    @classmethod
+    def update_or_create(cls, data, surrogate=True):
+        pk_props = cls.__mapper__.primary_key
+        # if all pk are present and not None
+        if not [1 for p in pk_props if data.get(p.key) is None]:
+            pk_tuple = tuple([data[prop.key] for prop in pk_props])
+            record = cls.query.get(pk_tuple)
+            if record is None:
+                if surrogate:
+                    raise Exception("cannot create surrogate with pk")
+                else:
+                    record = cls()
+        else:
+            if surrogate:
+                record = cls()
+            else:
+                raise Exception("cannot create non surrogate without pk")
+        record.from_dict(data)
+        return record
+
+
 Base = declarative_base(cls=BaseModel)
 Base.query = DBSession.query_property()
+
+
+__all__ = ["DBSession", "Base"]
