3a390e9e226ff12ed35d89e2dcb91e400f0ab932
Endre Karlson <endre.karlson@gmail.com>
2012-06-27 08:57:53 +0200

    Update tests and menu code

--- bookie/tests/test_helpers_menu.py
+++ bookie/tests/test_helpers_menu.py
@@ -6,17 +6,21 @@ from mock import MagicMock
 
 from bookie.testing import Dummy
 from bookie.testing import DummyRequest
+from bookie.views.helpers.menu import Menu, MenuItem
 
 
 MENU = {"title": "Home", "url": "#", "children": [
         {"title": "child1", "url": "child1"},
         {"title": "child2", "url": "child2", "children": [
             {"title": "subchild", "url": "subchild"}]
-        }]}
+        },
+        {"title": "child3", "url": "child3", "children": [
+            {"title": "subchild", "url": "subchild"}]}
+        ]}
+
 
 class TestMenuItem(unittest.TestCase):
     def make(self, menu=MENU):
-        from bookie.views.helpers.menu import MenuItem
         return MenuItem(Dummy(), DummyRequest(), **menu)
 
     def test_menu_structure(self):
@@ -32,13 +36,26 @@ class TestMenuItem(unittest.TestCase):
         self.assertEquals(m.children[1].url, "child2")
         self.assertEquals(m.children[1].children[0].title, "subchild")
         self.assertEquals(m.children[1].children[0].url, "subchild")
+        self.assertEquals(m.children[2].title, "child3")
+        self.assertEquals(m.children[2].url, "child3")
+        self.assertEquals(m.children[2].children[0].title, "subchild")
+        self.assertEquals(m.children[2].children[0].url, "subchild")
 
-    def test_menu_html(self):
-        """
-        Test the top level for now
-        """
+    def test_menu_is_parents(self):
         m = self.make()
-        self.assertEquals(m.__html__(), """<li><a href="#">Home</a></li>""")
+        self.assertEquals(m.is_parent, True)
+        self.assertEquals(m.children[0].is_parent, False)
+        self.assertEquals(m.children[1].is_parent, True)
+        self.assertEquals(m.children[2].is_parent, True)
+
+class TestMenu(unittest.TestCase):
+    def make(self, menu=MENU):
+        return Menu(Dummy(), DummyRequest, menu)
+
+    def test(self):
+        m = self.make()
+        for i in m.items.items():
+            print i.title, i.is_parent
 
 
 if __name__ == '__main__':
--- bookie/views/helpers/menu.py
+++ bookie/views/helpers/menu.py
@@ -1,12 +1,15 @@
-import itertools
+from itertools import chain
+import os.path
 import re
 
+from mako.template import Template
+from mako.lookup import TemplateLookup
 
-def item(**obj):
-    if isinstance(data, MenuItem):
-        return obj
-    else:
-        return MenuItem(**obj)
+
+def get_template(template):
+    template_dir = os.path.dirname(__file__) + "/templates/"
+    lookup = TemplateLookup(directories=[template_dir])
+    return lookup.get_template(template)
 
 
 class MenuItem(object):
@@ -20,26 +23,20 @@ class MenuItem(object):
     :param url: The href / url for this menu item
     :param icon: A Icon to prepend to the text
 
+    :param children: A list of children dicts
     :param parent: The parent of this item
-    :param children: This items children
     """
-    def __init__(self, context, request, parent=None, children=[], title=None,
-            url=None, icon=None):
-        self.context = context
-        self.request = request
+    def __init__(self, context, request, parent=None, children=[], title=None, url=None, icon=None):
+        self.context, self.request = context, request
 
         self.parent = parent
-        self.children = [self.create(context, request, parent=self, **i) for i in children]
+        self.children = [self.__class__(context, request, parent=self, **i) for i in children]
 
-        self.title = title
+        self.title, self.url, = title, url
         self.url = url
         if icon and not icon.startswith("icon-"):
             icon = "icon-" + icon
         self.icon = icon
-
-    @classmethod
-    def create(cls, context, request, **data):
-        return cls(context, request, **data)
 
     @property
     def is_active(self):
@@ -48,7 +45,28 @@ class MenuItem(object):
         """
         return self.request.url == self.url
 
+    @property
+    def is_parent(self):
+        return len(self.children) > 0
+
+    def levels(self):
+        i = 0
+        c = self
+        while (getattr(c, "parent", False)):
+            c = c.parent
+            i += 1
+
+    def __iter__(self):
+        return iter(self.children)
+
+    def items(self):
+        yield self
+        for item in self:
+            for child_item in item.items():
+                yield child_item
+
     def __html__(self):
+        return get_template(self.template).render(item=self)
         html = """<li%s><a href="%s">%s%s</a></li>"""
         icon_html = '<i class=%s></i>' % self.icon if self.icon else ''
         # NOTE: Need to add support for menu header here (sidebar)
@@ -58,3 +76,26 @@ class MenuItem(object):
         li_cls = ' class="%s"' % " ".join(li_cls) \
             if len(li_cls) > 0 else ''
         return html % (li_cls, self.url, icon_html, self.title)
+
+
+class Menu(object):
+    """
+    A Menu
+    """
+    template = None
+    def __init__(self, context, request, struct):
+        self.items = MenuItem(context, request, **struct)
+
+    def __iter__(self):
+        return self.items.items()
+
+    def __html__(self):
+        return get_template(self.template).render(menu=self)
+
+
+class Dropdown(Menu):
+    template = "dropdown.mako"
+
+
+class Sidebar(Menu):
+    template = "sidebar.mako"
