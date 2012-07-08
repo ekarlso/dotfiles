0a3bd0047851d09d0bcb878b364833a027626409
Endre Karlson <endre.karlson@gmail.com>
2012-07-08 22:44:06 +0200

    Add quick links and update sidebar links
    Change-Id: I0e97f5ba917658c9beec9c8a99f6042faf6c8859

--- bookie/views/retailer.py
+++ bookie/views/retailer.py
@@ -17,17 +17,30 @@ from .helpers import menu_item, get_nav_data, get_url, create_anchor, \
     wrap_td
 
 
-def links(request):
+def sidebar_links(request):
     d = get_nav_data(request)
-    c = []
-    c.append({"value": "Dashboard", "view_name": "retailer_dashboard",
+    links = []
+    links.append({"value": "Dashboard", "view": "retailer_dashboard",
         "view_kw": d})
-    return [{"value": "Navigation", "children": c}]
+    return [{"value": "Navigation", "children": links}]
+
+
+def quick_links(request):
+    d = get_nav_data(request)
+
+    links = []
+    links.append({"icon": "plus", "value": _("New Booking"),
+        "view": "booking_add", "view_kw": d})
+    links.append({"value": _("New Entity"), "icon": "plus",
+        "view": "entity_add", "view_kw": d})
+    links.append({"value": _("New User"), "icon": "plus",
+        "view": "tenant_user_add", "view_kw": d})
+    return {"children": links}
 
 
 @view_config(route_name="retailer_dashboard", renderer="retailer_dashboard.mako")
 def dashboard(context, request):
-    return {"nav_data": links(request)}
+    return {"nav_data": sidebar_links(request), "nav_quick": quick_links(request)}
 
 
 def includeme(config):
