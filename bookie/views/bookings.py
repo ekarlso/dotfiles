95c4f0b4ae4530279139c9e155b6f9baa6cce92e
Endre Karlson <endre.karlson@gmail.com>
2012-07-17 13:50:13 +0200

    Needed views for bookings
    Change-Id: I2c4cd66a8749144959a565f5d7731d6ce064f8ea

--- bookie/views/bookings.py
+++ bookie/views/bookings.py
@@ -1,5 +1,6 @@
+import ipdb
 import logging
-import pdb
+import re
 
 import colander
 import deform
@@ -9,17 +10,22 @@ from deform.widget import AutocompleteInputWidget, CheckedPasswordWidget, \
 from pyramid.httpexceptions import HTTPFound, HTTPNotFound
 from pyramid.exceptions import Forbidden
 from pyramid.request import Request
+from pyramid.threadlocal import get_current_request
 from pyramid.view import view_config
+from sqlalchemy.orm import exc
 
 from .. import models
 from ..utils import _, camel_to_name, name_to_camel
 from .helpers import AddFormView, EditFormView, mk_form
 from .helpers import PyramidGrid, column_link, wrap_td
-from .helpers import menu_item, menu_came_from, get_nav_data, get_url, create_anchor
+from .helpers import menu_item, menu_came_from, get_nav_data
+from .helpers import form
 
 
 
 LOG = logging.getLogger(__name__)
+
+PATTERN = r"^(?P<brand>\S+): (?P<model>\S+) - (?P<produced>\d{4}) - (?P<identifier>\S+)$"
 
 
 def booking_actions(obj=None, request=None):
@@ -39,42 +45,145 @@ def booking_links(obj=None, request=None):
     return navigation
 
 
+def customer_validate(node, value):
+    request = get_current_request()
+    try:
+        models.Customer.get_by(name=value, retailer=request.group)
+    except exc.NoResultFound:
+        raise colander.Invalid(node, "Invalid Customer")
+
+
+def entity_validate(node, value):
+    request = get_current_request()
+    try:
+        models.Entity.get_by(name=value, retailer=request.group)
+    except exc.NoResultFound:
+        raise colander.Invalid(node, "Invalid entity")
+
+
+def location_validate(node, value):
+    request = get_current_request()
+    try:
+        models.Location.get_by(name=value, retailer=request.group)
+    except exc.NoResultFound:
+        raise colander.Invalid(node, "Invalid Location")
+
+
+
+def pre_render(request, schema):
+    """
+    Helper to populate the schema
+    """
+    entities = []
+    for e in models.Entity.all_by(retailer=request.group):
+        entities.append(dict(label=str(e.name), value=str(e.name)))
+    schema["entity"].widget.values = entities
+
+    schema["customer"].widget.values = [dict(label=c.name, value=c.name) \
+            for c in models.Customer.all_by(retailer=request.group)]
+
+    locations = models.Location.all_by(retailer=request.group)
+    location_data = [dict(label=l.name, value=l.name) for l in locations]
+    schema["start_location"].widget.values = location_data
+    schema["end_location"].widget.values = location_data
+    return schema
+
+
+def presave(obj, appstruct):
+    appstruct["entity"] = models.Entity.get_by(
+        name=appstruct["entity"], retailer=obj.request.group)
+
+    appstruct["customer"] = models.Customer.get_by(
+        name=appstruct["customer"], retailer=obj.request.group)
+
+    appstruct["start_location"] = models.Location.by_name(
+            appstruct["start_location"], obj.request.group)
+    appstruct["end_location"] = models.Location.by_name(
+            appstruct["end_location"], obj.request.group)
+
+    return appstruct
+
+
 class BookingSchema(colander.Schema):
+    """
+    A Schema for a booking
+    """
+    entity = colander.SchemaNode(
+        colander.String(),
+        validator=entity_validate,
+        title=_("Entity to book"),
+        widget=AutocompleteInputWidget())
+    customer = colander.SchemaNode(
+        colander.String(),
+        validator=customer_validate,
+        title=_("Customer"),
+        widget=AutocompleteInputWidget())
     price = colander.SchemaNode(
         colander.Int(),
         title=_("Price"))
-    end_at = colander.SchemaNode(
-        colander.DateTime(),
-        title=_("Ends At"))
+    start_location = colander.SchemaNode(
+        colander.String(),
+        validator=location_validate,
+        title=_("Start location"),
+        widget=AutocompleteInputWidget())
     start_at = colander.SchemaNode(
-        colander.DateTime(),
+        colander.DateTime(None),
         title=_("Starts At"))
+    end_location = colander.SchemaNode(
+        colander.String(),
+        validator=location_validate,
+        title=_("End location"),
+        widget=AutocompleteInputWidget())
+    end_at = colander.SchemaNode(
+        colander.DateTime(None),
+        title=_("Ends time"))
 
 
-class BookingAddForm(AddFormView):
+class BookingAddForm(form.AddFormView):
     item_type = _(u"Booking")
     buttons = (Button("add_booking", _("Add Booking")),
                 Button("cancel", _("Cancel")))
 
     def schema_factory(self):
-        return BookingSchema()
+        schema = BookingSchema()
+        pre_render(self.request, schema)
+        return schema
 
     def add_booking_success(self, appstruct):
         appstruct.pop('csrf_token', None)
+        presave(self, appstruct)
         obj = models.Booking().update(appstruct).save()
         self.request.session.flash(_(u"${title} added.",
             mapping=dict(title=obj.title)), "success")
-        location = get_url("entities_view", type=self.item_type.lower())
+        location = self.request.route_url(
+            "booking_overview", **get_nav_data(self.request))
         return HTTPFound(location=location)
 
+    @property
+    def cancel_url(self):
+        return self.request.route_url("booking_overview",
+                **get_nav_data(self.request))
 
-class BookingEditForm(EditFormView):
+
+class BookingForm(form.EditFormView):
+    def schema_factory(self):
+        schema = BookingSchema()
+        pre_render(self.request, schema)
+        return schema
+
+    @property
+    def cancel_url(self):
+        return self.request.route_url("booking_overview",
+                **get_nav_data(self.request))
+
     @property
     def success_url(self):
-        return self.request.url
+        return self.request.route_url("booking_overview",
+                **get_nav_data(self.request))
 
-    def schema_factory(self):
-        return BookingSchema()
+    def save_success(self, appstruct):
+        presave(self, appstruct)
+        return super(BookingForm, self).save_success(appstruct)
 
 
 @view_config(route_name="booking_add", permission="view",
@@ -84,22 +193,18 @@ def booking_add(context, request):
         extra={"sidebar_data": booking_actions(request=request)})
 
 
-@view_config(route_name="booking_edit", permission="edit",
-            renderer="edit.mako")
-def booking_edit(context, request):
-    obj = models.Order.filter_by(
-        deleted=False, id=request.matchdict["id"]).one()
-    return mk_form(BookingEditForm, obj, request,
-        extra=dict(sidebar_data=booking_actions(request=request)))
-
-
 @view_config(route_name="booking_view", permission="view",
-            renderer="view.mako")
-def booking_view(context, request):
+            renderer="booking_manage.mako")
+def booking_manage(context, request):
     obj = models.Booking.get_by(id=request.matchdict["id"])
+
+    form = mk_form(BookingForm, obj, request)
+    if request.is_response(form):
+        return form
+
     return {
         "sidebar_data": booking_actions(request=request),
-        "booking": obj}
+        "form": form}
 
 
 @view_config(route_name="booking_overview", permission="view",
@@ -110,11 +215,16 @@ def booking_overview(context, request):
     bookings = models.Booking.search(
         filter_by={"deleted": deleted, "retailer": request.group})
 
-    columns = models.Booking.exposed_attrs() + ["entity"]
-    grid = PyramidGrid(bookings, columns)
+    columns = ["id"] + models.Booking.exposed_attrs() + ["entity"]
+    grid = PyramidGrid(bookings, columns, request=request, url=request.current_route_url)
 
     grid.column_formats["entity"] = lambda cn, i, item: column_link(
-        request, item["entity"], "entity_view", view_kw=item.entity.to_dict())
+        request, unicode(item.entity), "entity_view", url_kw=item.entity.to_dict())
+
+    grid.column_formats["id"] = lambda cn, i, item: column_link(
+        request, "View", "booking_view", url_kw=item.to_dict())
+    grid.labels["id"] = ""
+    grid.exclude_ordering = ("id")
 
     return {
         "sidebar_data": booking_actions(request=request),
@@ -122,7 +232,7 @@ def booking_overview(context, request):
 
 
 def includeme(config):
-    config.add_route("booking_add", "/@{group}/booking/add")
-    config.add_route("booking_edit", "/@{group}/booking/{id}/edit")
-    config.add_route("booking_view", "/@{group}/booking/{id}/view")
-    config.add_route("booking_overview", "/@{group}/booking")
+    config.add_route("booking_add", "/g,{group}/booking/add")
+    config.add_route("booking_edit", "/g,{group}/booking/{id}/edit")
+    config.add_route("booking_view", "/g,{group}/booking/{id}/view")
+    config.add_route("booking_overview", "/g,{group}/booking")
