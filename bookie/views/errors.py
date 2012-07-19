e30a2f4cfe22482709cc3e0ca65d226714e869e0
Endre Karlson <endre.karlson@gmail.com>
2012-07-19 11:19:27 +0200

    Don't use NoResultFound
    Change-Id: I502e30582a634490e0ad4e8b3810fc6e3385e126

--- bookie/views/errors.py
+++ bookie/views/errors.py
@@ -1,10 +1,10 @@
 from pyramid.response import Response
 from pyramid.view import view_config
-from sqlalchemy.orm.exc import NoResultFound
+from pyramid import httpexceptions as exceptions
 
 
 
-@view_config(context=NoResultFound, renderer="err/404.mako")
+@view_config(context=exceptions.HTTPNotFound, renderer="err/404.mako")
 def sqla_404(exc, request):
     request.response.status_int = 404
     return {"sub_title": "Queried resource not found"}
